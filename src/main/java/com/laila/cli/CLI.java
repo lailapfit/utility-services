f38b0f8745fb63dd96c1597d984896f98abe86c8
Laila Fitriana <laila@Base22s-MacBook-Pro-2.local>
2019-12-10 15:58:21 -0500

    added sorted strings file method

--- src/main/java/com/laila/cli/CLI.java
+++ src/main/java/com/laila/cli/CLI.java
@@ -1,9 +1,14 @@
 package com.laila.cli;
 
+import java.io.FileNotFoundException;
+
+import com.laila.string.StringUtils;
+
 public class CLI {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		StringUtils util = new StringUtils();
+		util.sortStringsInFile("unsorted.txt", "sorted.txt");
 	}
 
 }
--- src/main/java/com/laila/string/StringUtils.java
+++ src/main/java/com/laila/string/StringUtils.java
@@ -1,5 +1,11 @@
 package com.laila.string;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class StringUtils {
 	
 	//generate cron expression with String hh:mm
@@ -11,7 +17,29 @@ public class StringUtils {
 			 minute = time.substring(3, 5);
 		 }		 
 		 return String.format("%1$s %2$s %3$s %4$s %5$s %6$s", "0", hour, minute, "?", "*", "*");
-
-	 }
+	}
+	
+	@SuppressWarnings("resource")
+	public void sortStringsInFile(String unsorted, String sorted) throws FileNotFoundException {
+		File unsortedIncomingFile = new File(unsorted);
+		File sortedFileOutput = new File(sorted);
+		
+		PrintWriter printWriter = new PrintWriter(sortedFileOutput.getAbsoluteFile());
+		Scanner incoming = new Scanner(unsortedIncomingFile);
+		String[] unsortedStrings = null;
+		
+		while(incoming.hasNextLine()) {
+			unsortedStrings = incoming.nextLine().split(" ");
+		}
+		
+		Arrays.sort(unsortedStrings, (a, b) -> Integer.compare(a.length(), b.length()));
+		
+		for(String u: unsortedStrings) {
+			printWriter.write(u + "\n");
+		}
+		
+		printWriter.flush();
+	}
+	
 
 }
